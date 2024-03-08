d03794fdf75fb78943e875a7b003c8705432ac94
Manav Sureshbabu <manav.sureshbabu2022@vitstudent.ac.in>
2024-03-08 22:12:08 +0530

    V2

--- WEBDEV/LinguaConnect/models.py
+++ WEBDEV/LinguaConnect/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from datetime import datetime,date
 
 # Create your models here.
+class Teacher_Credentials(models.Model):
+    # id = models.AutoField(unique=True,blank=False,auto_created=True,primary_key=True)
+    name = models.CharField(max_length=255,blank=False)
+    emp_id = models.CharField(max_length=20,blank=False,default="Not Given")
+    email = models.CharField(max_length=255,default='Not Given')
+    phone = models.CharField(max_length=50,default='Not Given',blank=False)
+    u_id = models.CharField(max_length=255,unique=True,blank=False,primary_key=True)
+    password = models.CharField(max_length=255,default='1234')
+    exp = models.CharField(max_length=255,blank=False,default='Advanced')
+    dept = models.CharField(max_length=255,default='Client',blank=False)
+    status = models.CharField(max_length=50,default='Active',auto_created=True,blank=False)
+
+
+class Student_Credentials(models.Model):
+    # id = models.AutoField(unique=True,blank=False,auto_created=True,primary_key=True)
+    name = models.CharField(max_length=255,blank=False)
+    student_id = models.CharField(max_length=20,blank=False,default="Not Given")
+    email = models.CharField(max_length=255,default='Not Given')
+    phone = models.CharField(max_length=50,default='Not Given',blank=False)
+    u_id = models.CharField(max_length=255,unique=True,blank=False,primary_key=True)
+    password = models.CharField(max_length=255,default='1234')
+    status = models.CharField(max_length=50,default='Active',auto_created=True,blank=False)
