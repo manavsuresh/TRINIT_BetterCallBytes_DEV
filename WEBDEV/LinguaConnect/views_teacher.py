40393eedd091ff2d2ce92f9cb453d209fd24c7bb
manavsuresh <manavsuresh.04@gmail.com>
2024-03-09 10:14:54 +0530

    Basic Changes

---
+++ WEBDEV/LinguaConnect/views_teacher.py
@@ -0,0 +1,11 @@
+from datetime import date
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from .models import Student_Credentials as SCreds
+from .models import Teacher_Credentials as TCreds
+from . import views
+from django.conf import settings
+from django.core.mail import send_mail
+
+
