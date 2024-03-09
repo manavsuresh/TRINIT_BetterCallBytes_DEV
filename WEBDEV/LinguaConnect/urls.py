e2f3a432b7a9ee541c4f3192c176a839340f1b42
Manav Sureshbabu <manav.sureshbabu2022@vitstudent.ac.in>
2024-03-09 10:16:17 +0530

    DB changes

--- WEBDEV/LinguaConnect/urls.py
+++ WEBDEV/LinguaConnect/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     path('teacher/login_check/',log.login_check_teacher,name='teacher_check'),
     path('register/',log.register,name='register'),
     path('register/process/',log.process_register,name='process'),
-    path('teacher/dashboard/',teacher.dashboard,name="Teacher_Dashboard")
+    path('teacher/dashboard/',teacher.dashboard,name="Teacher_Dashboard"),
     path('teacher/dashboard/',student.dashboard,name="Student_Dashboard")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
