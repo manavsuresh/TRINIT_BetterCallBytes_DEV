8a4e1d44074ef054339095de9f779a4606c4b818
manavsuresh <manavsuresh.04@gmail.com>
2024-03-09 10:42:27 +0530

    .

--- WEBDEV/LinguaConnect/urls.py
+++ WEBDEV/LinguaConnect/urls.py
@@ -13,7 +13,11 @@ urlpatterns = [
     path('student/login_check/',log.login_check_student,name='student_check'),
     path('teacher/login_check/',log.login_check_teacher,name='teacher_check'),
     path('register/',log.register,name='register'),
-    path('teacher/register_edit/',log.edit_register_student,name='student_regedit'),
+    path('teacher/register_edit/<str:u_id>',log.edit_register_student,name='student_regedit'),
+    path('teacher/register_edit/done/<str:u_id>',log.edit_register_process_student,name='student_regedit_process'),
+
+    path('teacher/register_edit/<str:u_id>',log.edit_register_student,name='teacher_regedit'),
+    path('teacher/register_edit/done/<str:u_id>',log.edit_register_process_student,name='teacher_regedit_process'),
     path('register/process/',log.process_register,name='process'),
     path('teacher/dashboard/',teacher.dashboard,name="Teacher_Dashboard"),
     path('teacher/dashboard/',student.dashboard,name="Student_Dashboard")
