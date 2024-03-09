c8e030941494c3d1fc8d8969e533f0b030d37a59
manavsuresh <manavsuresh.04@gmail.com>
2024-03-09 18:25:47 +0530

    models Changes

--- WEBDEV/LinguaConnect/models.py
+++ WEBDEV/LinguaConnect/models.py
@@ -3,21 +3,16 @@ from datetime import datetime,date
 
 # Create your models here.
 class Teacher_Credentials(models.Model):
-    # id = models.AutoField(unique=True,blank=False,auto_created=True,primary_key=True)
     name = models.CharField(max_length=255,blank=False)
-    emp_id = models.CharField(max_length=20,blank=False,default="Not Given")
+    teacher_id = models.CharField(max_length=20,blank=False,default="Not Given")
     email = models.CharField(max_length=255,default='Not Given')
     phone = models.CharField(max_length=50,default='Not Given',blank=False)
     u_id = models.CharField(max_length=255,unique=True,blank=False,primary_key=True)
     password = models.CharField(max_length=255,default='1234')
-    exp = models.CharField(max_length=255,blank=False,default='Advanced')
-    budget = models.CharField(max_length=255,blank=False,default='0')
-    meeting_length = models.CharField(max_length=255,blank=False,default='0000') #[45,60,90,120]
     status = models.CharField(max_length=50,default='Active',auto_created=True,blank=False)
 
 
 class Student_Credentials(models.Model):
-    # id = models.AutoField(unique=True,blank=False,auto_created=True,primary_key=True)
     name = models.CharField(max_length=255,blank=False)
     student_id = models.CharField(max_length=20,blank=False,default="Not Given")
     email = models.CharField(max_length=255,default='Not Given')
@@ -25,3 +20,19 @@ class Student_Credentials(models.Model):
     u_id = models.CharField(max_length=255,unique=True,blank=False,primary_key=True)
     password = models.CharField(max_length=255,default='1234')
     status = models.CharField(max_length=50,default='Active',auto_created=True,blank=False)
+    notes = models.CharField(max_length=2000,default="",auto_created=True,blank=False)
+
+class Classes(models.Model):
+    CLASS_ID = models.CharField(max_length=255,blank=False,unique=True,primary_key=True)
+    Language = models.CharField(max_length=255,blank=False,default="-")
+    Price = models.CharField(max_length=255,blank=False,default="0")
+    Timing = models.CharField(max_length=255,blank=False,default="-")
+    Class_Date = models.CharField(max_length=255,blank=False,default=date.today(),auto_created=True)
+    Teacher = models.CharField(max_length=255,blank=False,default="-")
+    Students = models.CharField(max_length=255,blank=False,default="0",auto_created=True)
+
+class Teacher(models.Model):
+    Teacher_ID = models.CharField(max_length=255,blank=False,unique=True,primary_key=True)
+    Language = models.CharField(max_length=255,blank=False,default="-")
+    Profeciency = models.CharField(max_length=255,blank=False,default="Advanced")
+
