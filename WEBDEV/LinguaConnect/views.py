31fbb097e522fda66badda2a3896111fd836410a
Manav Sureshbabu <manav.sureshbabu2022@vitstudent.ac.in>
2024-03-09 17:37:46 +0530

    static

--- WEBDEV/LinguaConnect/views.py
+++ WEBDEV/LinguaConnect/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from datetime import date
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from . import views_student
+from . import views_teacher
+from . import views_login
 
-# Create your views here.
+
+
+#To get ip address
+def get_ip(request):
+    user_ip_address = request.META.get('HTTP_X_FORWARDED_FOR')
+    if user_ip_address:
+        ip = user_ip_address.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
--- WEBDEV/WEBDEV/settings.py
+++ WEBDEV/WEBDEV/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=@@@7gfp80-0yisdzp0$jw6p3+2h1w(2e@c^o9ybnuwmw4hje@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
